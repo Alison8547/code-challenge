41ef63c97eedfe4d776acfc5806757aa1429a245
Alison8547 <alisonailson100@hotmail.com>
2023-01-09 22:49:54 -0300

    primeiros desafios

---
+++ src/main/java/easy/JavaStdinStdout1.java
@@ -0,0 +1,14 @@
+package easy;
+
+import java.util.Scanner;
+
+public class JavaStdinStdout1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+        int number3 = scanner.nextInt();
+
+        System.out.printf("%d%n%d%n%d%n", number1, number2, number3);
+    }
+}
