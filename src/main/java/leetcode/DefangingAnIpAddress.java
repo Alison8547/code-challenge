ba113943b37750563fd67fa7a7600bd0bd87e95c
Alison8547 <alisonailson100@hotmail.com>
2023-09-07 19:31:07 -0300

    feat: Defanging an Ip Address

---
+++ src/main/java/leetcode/DefangingAnIpAddress.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+public class DefangingAnIpAddress {
+    public static void main(String[] args) {
+        System.out.println(defangIPaddr("255.100.50.0"));
+    }
+
+    public static String defangIPaddr(String address) {
+        return address.replace(".", "[.]");
+    }
+}
