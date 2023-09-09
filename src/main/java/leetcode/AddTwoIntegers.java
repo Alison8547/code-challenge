ebf9fb6f154e11da95c0ca852d95daffd964480d
Alison8547 <alisonailson100@hotmail.com>
2023-09-09 12:59:52 -0300

    feat: Add two integers

---
+++ src/main/java/leetcode/AddTwoIntegers.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class AddTwoIntegers {
+    public static void main(String[] args) {
+        System.out.println(sum(-10,4));
+    }
+
+    public static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+}
