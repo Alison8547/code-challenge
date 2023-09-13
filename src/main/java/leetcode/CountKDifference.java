efc8ed0256b6bb72b2e0b68bd7343b4412b50f19
Alison8547 <alisonailson100@hotmail.com>
2023-09-13 17:49:47 -0300

    feat: Count k difference

---
+++ src/main/java/leetcode/CountKDifference.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+public class CountKDifference {
+    public static void main(String[] args) {
+        System.out.println(countKDifference(new int[]{1, 2, 2, 1}, 1));
+    }
+
+    public static int countKDifference(int[] nums, int k) {
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (Math.abs(nums[i] - nums[j]) == k) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
