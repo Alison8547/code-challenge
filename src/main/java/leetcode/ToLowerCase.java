2e60de7653abd0eadd282e39936fa9c05d8ec132
Alison8547 <alisonailson100@hotmail.com>
2023-10-08 20:38:12 -0300

    feat: toLowerCase

---
+++ src/main/java/leetcode/ToLowerCase.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+public class ToLowerCase {
+    public static void main(String[] args) {
+        System.out.println(toLowerCase("Hello"));
+    }
+
+    public static String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
